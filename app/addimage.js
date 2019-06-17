bc0bcc7d3d23d3acaca33d70820a056ddf624175
Acivii <Acivii0312@163.com>
2019-06-17 14:53:11 +0800

    first demo

---
+++ app/addimage.js
@@ -0,0 +1,13 @@
+function aaa(){
+// addImage.js
+let smallImg = document.createElement('img')
+// 必须 require 进来
+smallImg.src = require('../images/1.jpg')
+document.body.appendChild(smallImg)
+
+let bigImg = document.createElement('img')
+bigImg.src = require('../images/2.jpg')
+document.body.appendChild(bigImg)
+}
+
+export default aaa
---
+++ app/index.js
@@ -0,0 +1,5 @@
+import sum from './sum'
+import images from './addimage'
+import '../css/style.scss'
+console.log(sum(1, 2))
+images()
